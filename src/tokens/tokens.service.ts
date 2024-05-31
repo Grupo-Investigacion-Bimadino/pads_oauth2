44d72b5285a0c1c9c9d83411cb281ed90255175d
DANIELDORIA543 <danieldoria543@gmail.com>
2024-05-31 00:26:51 -0500

    CRUD TOKENS

--- src/tokens/tokens.service.ts
+++ src/tokens/tokens.service.ts
@@ -5,22 +5,57 @@ import { UpdateTokenDto } from './dto/update-token.dto';
 @Injectable()
 export class TokensService {
   create(createTokenDto: CreateTokenDto) {
-    return 'This action adds a new token';
+    return createTokenDto;
   }
 
   findAll() {
-    return `This action returns all tokens`;
+    return [
+      {
+
+        id: 1,
+        token: 6655,
+        duration: "05:00 minutes",
+        Encrypted_algorithm: 123456,
+        data: "nombre y apellido"
+
+      },
+
+      {
+        id: 2,
+        token: 12345,
+        duration: "09:00 minutes",
+        Encrypted_algorithm: 97654744,
+        data: "edad y sexo"
+
+      }
+
+    ]
+      
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} token`;
+    return {
+
+      id,
+      token: 6655,
+      duration: "05:00 minutes",
+      Encrypted_algorithm: 123456,
+      data: "nombre y apellido"
+
+    };
   }
 
   update(id: number, updateTokenDto: UpdateTokenDto) {
-    return `This action updates a #${id} token`;
+    return {
+      id,
+      updateTokenDto,
+    };
   }
 
   remove(id: number) {
-    return `This action removes a #${id} token`;
+    return {
+
+      id,
+    };
   }
 }

c1d98ea650a5e99c63e4fa18a12205729b0d7425
Jabib Roberto Manzur Lemos <143450503+jabibManz@users.noreply.github.com>
2024-06-29 16:00:12 -0500

    logos.module.ts
    actualizacion

--- src/logos/logos.module.ts
+++ src/logos/logos.module.ts
@@ -1,16 +1,34 @@
-import { Module } from '@nestjs/common';
-import { logosService } from './logos.service';
-import { logosController } from './logos.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { logosSchema } from './Schemas/logos';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { LogosService } from './logos.service';
+import { CreateLogosDto } from './dto/create-logos.dto';
+import { UpdateLogosDto } from './dto/update-logos.dto';
 
+@Controller('logos')
+export class LogosController {
+  constructor(private readonly logosService: LogosService) {}
 
+  @Post()
+  create(@Body() createLogosDto: CreateLogosDto) {
+    return this.logosService.create(createLogosDto);
+  }
 
-@Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'logos', schema: logosSchema }]),
-  ],
-  controllers: [logosController],
-  providers: [logosService],
-})
-export class logosModule {}
+  @Get()
+  findAll() {
+    return this.logosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.logosService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateLogosDto: UpdateLogosDto) {
+    return this.logosService.update(+id, updateLogosDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.logosService.remove(+id);
+  }
+}

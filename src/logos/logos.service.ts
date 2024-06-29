d727d5fa49aa6c428c61555ba1546429342d2567
Jabib Roberto Manzur Lemos <143450503+jabibManz@users.noreply.github.com>
2024-06-29 15:55:22 -0500

    logos.service.ts
    actulizacion

--- src/logos/logos.service.ts
+++ src/logos/logos.service.ts
@@ -1,56 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import { CreateLogoDto } from './dto/create-logo.dto';
-import { UpdateLogoDto } from './dto/update-logo.dto';
-
+import { CreatelogosDto } from './dto/create-logos.dto';
+import { UpdatelogosDto } from './dto/update-logos.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { logos } from './schemas/logos';
 @Injectable()
-export class LogosService {
-  create(createLogoDto: CreateLogoDto) {
-    return createLogoDto;
+export class logosService {
+  constructor(@InjectModel(logos.name) private logosModel: Model<logos>) {}
+
+  async create(createlogosDto: CreatelogosDto) {
+    const createdlogos = new this.logosModel(createlogosDto);
+    return createdlogos.save();
   }
 
   findAll() {
-    return [
+    return this.logosModel.find().exec();
 
-      {
-        id: 23,
-        url_logo: "google.com",
-        format: "jpg",
-        id_logos: 2
-      },
-      {
-        id: 24,
-        url_logo: "facebook.com",
-        format : "png",
-        id_logos: 10
-      },
-      {
-        id: 30,
-        url_logo: "x.com",
-        format: "png",
-        id_logos: 5
-      }
-    ];
   }
-
-  findOne(id: number) {
-    return {
-      id,
-      url_logo: "google.com",
-      format: "jpg",
-      id_logos: 3
+    findOne(id: string) {
+      return this.logosModel.findById(id).exec();
+    }
+  
+    update(id: string, updatelogosDto: UpdatelogosDto) {
+      return this.logosModel
+        .findByIdAndUpdate(id, updatelogosDto, {
+          new: true,
+        })
+        .exec();
+    }
+  
+    remove(id: string) {
+      return this.logosModel.findByIdAndDelete(id).exec();
     }
   }
 
-  update(id: number, updateLogoDto: UpdateLogoDto) {
-    return {
-      id,
-      updateLogoDto
-    };
-  }
-
-  remove(id: number) {
-    return {
-      id,
-    };
-  }
-}
